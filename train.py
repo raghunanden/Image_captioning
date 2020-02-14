00c178b252011686ea52dc19de593ba0ea6bf7c7
raghunanden <raghav.sharma0002@gmail.com>
2020-02-14 12:33:35 +0530

    changing_paths

--- train.py
+++ train.py
@@ -11,7 +11,7 @@ from utils import *
 from nltk.translate.bleu_score import corpus_bleu
 
 # Data parameters
-data_folder = os.path.join("Flicker8k_Dataset")  # folder with data files saved by create_input_files.py
+data_folder = os.path.join("../","Flicker8k_Dataset")  # folder with data files saved by create_input_files.py
 data_name = 'flickr8k_5_cap_per_img_5_min_word_freq'  # base name shared by data files
 
 # Model parameters
@@ -26,7 +26,7 @@ cudnn.benchmark = True  # set to true only if inputs to model are fixed size; ot
 start_epoch = 0
 epochs = 120  # number of epochs to train for (if early stopping is not triggered)
 epochs_since_improvement = 0  # keeps track of number of epochs since there's been an improvement in validation BLEU
-batch_size = 1
+batch_size = 32
 workers = 0  # for data-loading; right now, only 1 works with h5py
 encoder_lr = 1e-4  # learning rate for encoder if fine-tuning
 decoder_lr = 4e-4  # learning rate for decoder
